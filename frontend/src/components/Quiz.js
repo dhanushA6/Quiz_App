ff49143aaa64b5916af04398f39aa91bfe3d7b95
Dhanushanadhan <dhanush2210530@ssn.edu.in>
2025-02-24 15:43:19 +0530

    api url changed for frontend

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -261,7 +261,7 @@ const Quiz = () => {
   useEffect(() => {
     const fetchQuizzes = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/quiz');
+        const response = await axios.get('https://quiz-server-one-liard.vercel.app/api/quiz');
         setQuizzes(response.data);
       } catch (error) {
         console.error('Error fetching quizzes:', error);
